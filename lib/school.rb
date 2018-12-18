b10a394b2c480e0dcf9ebca0c9a8968e92e46617
Rachel Hawa <rachel.hawa@gmail.com>
2018-12-18 00:51:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :roster, :student
+
+  def initialize(student)
+    @student = student
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_name)
+  end
+
+  def sort
+  end
+end
